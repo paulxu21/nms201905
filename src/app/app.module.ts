8f9f5906dfcc70eea1d704c3d2b0ce35da2f2b7d
paulxu21 <paul_xu21@hotmail.com>
2019-04-29 19:40:22 +0800

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ShareModule } from './share/share.module';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ShareModule,
     AppRoutingModule
   ],
   providers: [],
